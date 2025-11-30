17032a621df307e8de0b3f87234e7d4a3783c127
sigdelsanjog <sigdelsanjog@gmail.com>
2025-11-30 23:23:05 +0545

    Django project didn't work switching to next.js

--- api/index.py
+++
@@ -1,4 +0,0 @@
-from resume_blog.asgi import application as app
-
-# Vercel Python serverless function expects a callable named `app`.
-# This imports the Django ASGI application so Vercel can invoke it.
